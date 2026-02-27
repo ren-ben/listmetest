c2419c808aa03c4a2b2947a3bca6f1b7fb1ff8cb
Oliwier Przewlocki <oprzewlocki@student.tgm.ac.at>
2026-02-27 22:38:51 +0100

    fix computed import

--- listme-frontend/src/components/common/HKBackground.vue
+++ listme-frontend/src/components/common/HKBackground.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { computed } from 'vue'
 import { Teleport } from 'vue'
 import { useThemeStore } from '../../stores/theme'
 
--- listme-frontend/src/components/common/HelloKittyMascot.vue
+++ listme-frontend/src/components/common/HelloKittyMascot.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import { Teleport } from 'vue'
 import { useThemeStore } from '../../stores/theme'
 
 const themeStore = useThemeStore()
